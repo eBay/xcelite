650ec0fcc31fcf8b8579fe68e8dfd4aa33d11cf9
iSnow <139699+iSnow@users.noreply.github.com>
2018-09-19 20:34:40 +0200

    updated to v.1.2.3 from DEV

--- src/test/java/com/ebay/xcelite/writer/AbstractTestBaseForWriterTests.java
+++ src/test/java/com/ebay/xcelite/writer/AbstractTestBaseForWriterTests.java
@@ -18,7 +18,7 @@ import java.util.*;
 
 public class AbstractTestBaseForWriterTests{
     // set to true to look at the resulting spreadsheet files
-    static boolean writeToFile = true;
+    static boolean writeToFile = false;
     static XSSFWorkbook workbook;
 
     @SneakyThrows
